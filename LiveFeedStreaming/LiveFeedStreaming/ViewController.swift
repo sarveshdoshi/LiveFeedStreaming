bd4e91eb28ea9d5510ee5edf789ec7109679019c
Sarvesh Doshi <sarvesh.doshi@in10media.com>
2024-12-17 23:50:42 +0530

    initial Project Commit with base project version changes.

---
+++ LiveFeedStreaming/LiveFeedStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveFeedStreaming
+//
+//  Created by Sarvesh Doshi on 17/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
