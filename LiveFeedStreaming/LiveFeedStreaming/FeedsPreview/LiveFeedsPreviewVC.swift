fc63e38c548fe1ec9fa43ed3a3083847cd136b13
Sarvesh Doshi <sarvesh.doshi@in10media.com>
2024-12-19 00:29:23 +0530

    implemented the code for reel scroll.

--- LiveFeedStreaming/LiveFeedStreaming/FeedsPreview/LiveFeedsPreviewVC.swift
+++ LiveFeedStreaming/LiveFeedStreaming/FeedsPreview/LiveFeedsPreviewVC.swift
@@ -18,7 +18,29 @@ class LiveFeedsPreviewVC: UIViewController {
     // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.register(UINib(nibName: "LiveFeedsTVC", bundle: .main), forCellReuseIdentifier: "LiveFeedsTVC")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isPagingEnabled = true
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.insetsContentViewsToSafeArea = false
     }
 
+}
+
+extension LiveFeedsPreviewVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return viewModel.feedData?.videos?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = viewModel.feedData?.videos?[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LiveFeedsTVC") as! LiveFeedsTVC
+        cell.passData(data: data, index: indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.height
+    }
 }
--- LiveFeedStreaming/LiveFeedStreaming/FeedsPreview/LiveFeedsTVC/LiveFeedsTVC.swift
+++ LiveFeedStreaming/LiveFeedStreaming/FeedsPreview/LiveFeedsTVC/LiveFeedsTVC.swift
@@ -6,18 +6,131 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
+import AVKit
 
 class LiveFeedsTVC: UITableViewCell {
 
+    @IBOutlet weak var playerBGView: UIView!
+    @IBOutlet weak var overlayView: UIView!
+    
+    
+    let avPlayerController = AVPlayerViewController()
+    var data: Video?
+    var currentPlayedContentIndex = 0
+    
+    
+    deinit {
+        print("Deinit called.")
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        playAudioBackground()
+        addObservers()
     }
-
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setVideoPlayerlayout()
+    }
+    
+    
+   
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+    }
+    
+    func passData(data: Video?, index: Int) {
+        self.data = data
+        self.currentPlayedContentIndex = index
+        
+        if let urlStr = data?.video, let url = URL(string: urlStr) {
+            let asset = AVURLAsset(url: url)
+            let playerItem = AVPlayerItem(asset: asset)
+            avPlayerController.player?.actionAtItemEnd = .none
+            avPlayerController.player = AVPlayer(playerItem: playerItem)
+            avPlayerController.showsPlaybackControls = false
+            
+            self.overlayView.addSubview(avPlayerController.view)
+            self.setVideoPlayerlayout()
+            
+            avPlayerController.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            
+            overlayView.sendSubviewToBack(avPlayerController.view)
+            avPlayerController.player?.play()
+            avPlayerController.player?.volume = 1.0
+            if #available(iOS 16.0, *) {
+                avPlayerController.allowsVideoFrameAnalysis = false
+            }
+            
+        } else {
+            print("video url nil...")
+        }
+        
+    }
+    
+    func setVideoPlayerlayout() {
+        avPlayerController.view.translatesAutoresizingMaskIntoConstraints = false
+        avPlayerController.view.topAnchor.constraint(equalTo: playerBGView.topAnchor, constant: 0).isActive = true
+        avPlayerController.view.leadingAnchor.constraint(equalTo: playerBGView.leadingAnchor, constant: 0).isActive = true
+        avPlayerController.view.trailingAnchor.constraint(equalTo: playerBGView.trailingAnchor, constant: 0).isActive = true
+        avPlayerController.view.bottomAnchor.constraint(equalTo: playerBGView.bottomAnchor, constant: 0).isActive = true
+    }
+}
+
+extension LiveFeedsTVC {
+    private func playAudioBackground() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Error setting the AVAudioSession:", error.localizedDescription)
+        }
+    }
+    
+    func addObservers() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.willEnterForeground),
+                                               name: UIApplication.willEnterForegroundNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.didEnterBackground),
+                                               name: UIApplication.didEnterBackgroundNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.playerDidFinishPlaying),
+                                               name: .AVPlayerItemDidPlayToEndTime,
+                                               object: self.avPlayerController.player?.currentItem)
+    }
+    
+    @objc func didEnterBackground() {
+       
+        avPlayerController.player?.pause()
+    }
+    
+    @objc func willEnterForeground() {
+       
+        avPlayerController.player?.play()
+    }
+    
+    @objc func playerDidFinishPlaying(note: NSNotification) {
+        self.currentStoryPlay()
+    }
+    
+    private func currentStoryPlay() {
+        if let player = avPlayerController.player, let currentItem = player.currentItem {
+            avPlayerController.player?.pause()
+            avPlayerController.player?.seek(to: .zero)
+            avPlayerController.player?.replaceCurrentItem(with: currentItem)
+            avPlayerController.player?.play()
+        }
     }
     
 }
